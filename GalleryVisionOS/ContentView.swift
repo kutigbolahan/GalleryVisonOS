74638d7fa0d213719e50fd16e1fa57ae5c316826
kuti Gbolahan <kutigbolahan.ab@gmail.com>
2025-06-05 15:41:37 +0100

    second commit

--- GalleryVisionOS/ContentView.swift
+++ GalleryVisionOS/ContentView.swift
@@ -15,7 +15,7 @@ struct ContentView: View {
             Model3D(named: "Scene", bundle: realityKitContentBundle)
                 .padding(.bottom, 50)
 
-            Text("Hello, world!")
+            Text("Hello, worldrrr!")
         }
         .padding()
     }
