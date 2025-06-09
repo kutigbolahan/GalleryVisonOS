44facc2627c86cf3c922b2133f0acab8578f6652
kuti Gbolahan <kutigbolahan.ab@gmail.com>
2025-06-09 17:01:36 +0100

    added a model and updated with realitykit

---
+++ GalleryVisionOS/SphereView.swift
@@ -0,0 +1,18 @@
+//
+//  SphereView.swift
+//  GalleryVisionOS
+//
+//  Created by Kuti Gbolahan on 05/06/2025.
+//
+
+import SwiftUI
+
+struct SphereView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SphereView()
+}
